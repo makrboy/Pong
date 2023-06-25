c86847a8192fc2038a4d1e6b9110462164d76e5e
Jack Caswell <jack@creationix.com>
2023-06-24 22:45:39 -0500

    fixed the ai

--- pong.js
+++ pong.js
@@ -1,66 +1,80 @@
-let opPlayer = false;
-let playerPaddle, aiPaddle, ball, paddleWidth, paddleHeight, paddleSpeed, ballSize, ballSpeed, aiNapTime;
-let playerScore = 0;
-let aiScore = 0;
-let scoreColor;
-let targetScoreColor;
+let opPlayer = false
+let playerPaddle, aiPaddle, ball, paddleWidth, paddleHeight, paddleSpeed, ballSize, ballSpeed, aiNapTime
+let playerScore = 0
+let aiScore = 0
+let scoreColor = 255
+let gameover = false
+let fakeBalls = []
 
 function setup() {
-    createCanvas(windowWidth, windowHeight).id("canvas");
-    paddleWidth = windowWidth / 5;
-    paddleHeight = windowWidth / 50;
-    ballSize = windowWidth / 50;
-    ballSpeed = (windowHeight + windowWidth) / 2 / 100;
-    paddleSpeed = (windowHeight + windowWidth) / 2 / 50;
+    createCanvas(windowWidth, windowHeight).id("canvas")
+    paddleWidth = windowWidth / 5
+    paddleHeight = windowWidth / 50
+    ballSize = windowWidth / 50
+    ballSpeed = (windowHeight + windowWidth) / 2 / 100
+    paddleSpeed = (windowHeight + windowWidth) / 2 / 50
 
-    playerPaddle = new Paddle(paddleWidth, paddleHeight, paddleSpeed, true);
-    aiPaddle = new Paddle(paddleWidth, paddleHeight, paddleSpeed, false);
-    ball = new Ball(ballSize, ballSpeed);
-    scoreColor = color(0, 255, 0, 255);
-    targetScoreColor = color(0, 255, 0, 255);
+    playerPaddle = new Paddle(paddleWidth, paddleHeight, paddleSpeed, true)
+    aiPaddle = new Paddle(paddleWidth, paddleHeight, paddleSpeed, false)
+    ball = new Ball(ballSize, ballSpeed)
+
+    aiScore = 0
+    playerScore = 0
 }
 
 function draw() {
-    background(0);
-    playerPaddle.display();
-    playerPaddle.move(ball);
-    aiPaddle.display();
-    aiPaddle.move(ball);
-    ball.display();
-    ball.move();
-    ball.checkCollision(playerPaddle, aiPaddle);
-    displayScore();
+    background(0)
+    if (!gameover) {
+        playerPaddle.display()
+        playerPaddle.move(ball)
+        aiPaddle.display()
+        aiPaddle.move(ball)
+        ball.display()
+        ball.move()
+        ball.checkCollision(playerPaddle, aiPaddle)
+        displayScore()
+        for (let index in fakeBalls) {
+            let fakeBall = fakeBalls[index]
+            fill(0, 100, 0)
+            rect(
+                ball.x * fakeBall.mx + fakeBall.ox,
+                ball.y * fakeBall.my + fakeBall.oy,
+                fakeBall.size,
+                fakeBall.size
+            )
+        }
+    }
 }
 
 function displayScore() {
-    scoreColor = lerpColor(scoreColor, targetScoreColor, 0.05);
-    textAlign(CENTER, CENTER);
-    textSize((windowHeight + windowWidth) / 2 / 25);
-    fill(scoreColor);
-    text("Player: " + playerScore + '\nAI: ' + aiScore + "\nFirst to three wins", windowWidth / 2, windowHeight / 2);
+    scoreColor = Math.max(scoreColor - 5, 0)
+    textAlign(CENTER, CENTER)
+    textSize((windowHeight + windowWidth) / 2 / 25)
+    fill(color(0, scoreColor, 0))
+    text("Player: " + playerScore + '\nAI: ' + aiScore + "\nFirst to three wins", windowWidth / 2, windowHeight / 2)
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    resizeCanvas(windowWidth, windowHeight)
 }
 
 var touchPosition = {
     x: null,
     y: null
-};
+}
 
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchend', handleTouchEnd, false);
+document.addEventListener('touchstart', handleTouchStart, false)
+document.addEventListener('touchend', handleTouchEnd, false)
 
 function handleTouchStart(event) {
-    var touch = event.touches[0];
-    touchPosition.x = touch.clientX;
-    touchPosition.y = touch.clientY;
+    var touch = event.touches[0]
+    touchPosition.x = touch.clientX
+    touchPosition.y = touch.clientY
 }
 
 function handleTouchEnd(event) {
-    touchPosition.x = null;
-    touchPosition.y = null;
+    touchPosition.x = null
+    touchPosition.y = null
 }
 
 class Paddle {
@@ -76,8 +90,8 @@ class Paddle {
     }
 
     display() {
-        fill(0, 255, 0);
-        rect(this.x, this.y, this.w, this.h);
+        fill(0, 255, 0)
+        rect(this.x, this.y, this.w, this.h)
     }
 
     move(ball) {
@@ -86,10 +100,10 @@ class Paddle {
                 this.x = ball.x - this.w / 2
             } else {
                 if ((keyIsDown(LEFT_ARROW) || (touchPosition.x !== null && touchPosition.x < windowWidth / 2)) && this.x > 0) {
-                    this.x -= this.speed;
+                    this.x -= this.speed
                 }
                 if ((keyIsDown(RIGHT_ARROW) || (touchPosition.x !== null && touchPosition.x > windowWidth / 2)) && this.x < windowWidth - this.w) {
-                    this.x += this.speed;
+                    this.x += this.speed
                 }
             }
         } else {
@@ -114,23 +128,26 @@ function calculateBounce(vx, vy, cw, ch, bx, by) {
 
 class Ball {
     constructor(s, v) {
-        this.s = s;
-        this.initialSpeed = v;
-        this.vx = v;
-        this.vy = v;
-        this.speedIncrement = 0;
-        this.reset();
+        this.s = s
+        this.initialSpeed = v
+        this.vx = v
+        this.vy = v
+        this.speedIncrement = 0
+        this.reset(true)
     }
 
-    reset() {
-        if (Math.random() < .1) {
+    reset(first) {
+        this.vy > 0 ? playerScore++ : aiScore++
+        scoreColor = 255
+
+        if (Math.random() < .5) {
             this.x = Math.random() * (windowWidth - this.s)
-            this.y = this.s / 2 + paddleHeight * 2
+            this.y = this.s + paddleHeight * 2
             this.vx = this.initialSpeed * (Math.random() < .5 ? 1 : -1)
             this.vy = this.initialSpeed
         } else {
             this.x = Math.random() * (windowWidth - this.s)
-            this.y = windowHeight - this.s / 2 - paddleHeight * 2
+            this.y = windowHeight - this.s - paddleHeight * 2
             this.vx = this.initialSpeed * (Math.random() < .5 ? 1 : -1)
             this.vy = -this.initialSpeed
         }
@@ -139,19 +156,23 @@ class Ball {
         aiPaddle.speed = paddleSpeed
         aiPaddle.targetX = calculateBounce(this.vx, this.vy, windowWidth, windowHeight, this.x, this.y)
 
-        if (this.vy > 0) {
-            playerScore++;
-            targetScoreColor = color(0, 255, 0, 0);
-        } else {
-            aiScore++;
-            targetScoreColor = color(0, 255, 0, 0);
+        if (first) {
+            playerScore = 0
+            aiScore = 0
         }
-
-        scoreColor = color(0, 255, 0, 255);
+        if (playerScore == 0) {
+            aiPaddle.speed = paddleSpeed / 5
+        }
+        if (playerScore == 2) {
+            aiPaddle.speed = paddleSpeed * 5
+        }
+        if (aiScore == 3) {
+            end()
+        }
     }
 
     display() {
-        fill(0, 255, 0);
+        fill(0, 255, 0)
         rect(this.x, this.y, this.s, this.s)
     }
 
@@ -163,25 +184,56 @@ class Ball {
             this.vx *= -1
         }
 
-        if (this.y < 0 || this.y > windowHeight - this.s) {
+        if (this.y < 0 || (this.y > windowHeight - this.s && playerScore < 2)) {
             this.reset()
-            opPlayer = false
+        }
+
+        if (this.y > windowHeight - this.s && playerScore == 2) {
+            this.vy *= -1
+            for (let i = 0; i < Math.random() * 10; i++) {
+                fakeBalls.push({
+                    mx: Math.random() / 2 + .5,
+                    my: Math.random() / 2 + .5,
+                    ox: Math.random() * window.innerWidth - window.innerWidth / 2,
+                    oy: Math.random() * window.innerHeight - window.innerHeight / 2,
+                    size: Math.random() * ballSize * 5
+                })
+            }
+            aiPaddle.napTime = 0
+            aiPaddle.speed = paddleSpeed * 5
+            aiPaddle.targetX = calculateBounce(ball.vx, ball.vy, windowWidth, windowHeight, ball.x, ball.y)
         }
     }
 
     checkCollision(playerPaddle, aiPaddle) {
         if (this.y <= playerPaddle.h && this.x + this.s > playerPaddle.x && this.x < playerPaddle.x + playerPaddle.w) {
-            this.vy *= -1;
-            this.vx += this.vx < 0 ? -this.speedIncrement : this.speedIncrement;
-            this.vy += this.vy < 0 ? -this.speedIncrement : this.speedIncrement;
+            this.vy *= -1
+            this.vx += this.vx < 0 ? -this.speedIncrement : this.speedIncrement
+            this.vy += this.vy < 0 ? -this.speedIncrement : this.speedIncrement
         }
         if (this.y + this.s >= windowHeight - aiPaddle.h && this.x + this.s > aiPaddle.x && this.x < aiPaddle.x + aiPaddle.w) {
-            this.vy *= -1;
+            this.vy *= -1
             setTimeout(() => {
                 aiPaddle.targetX = calculateBounce(this.vx, this.vy, windowWidth, windowHeight, this.x, this.y)
-            }, aiPaddle.napTime);
+            }, aiPaddle.napTime)
+            if (playerScore == 2) {
+                ball.vx *= 1.01
+                ball.vy *= 1.01
+                playerPaddle.speed *= 1.01
+            }
             aiPaddle.napTime += 100
             aiPaddle.speed *= .9
         }
     }
+}
+
+function end() {
+    gameover = true
+    let reloadBox = document.getElementById('reloadBox')
+    reloadBox.textContent = 'The AI won\nClick to Restart'
+    reloadBox.classList.add('visible')
+
+    reloadBox.addEventListener('click', function () {
+        location.reload()
+    })
 }
